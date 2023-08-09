64a909b3867a8608bd29d9f77cbd2d614b72a3b0
Javed Hussein <jdhn42@gmail.com>
2023-08-09 12:40:12 -0400

    refactor(misc): update linting issues

--- resources/license.header.js
+++ resources/license.header.js
@@ -14,6 +14,3 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-export interface Equatable<T> {
-  equals: (other: T) => boolean;
-}
--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,4 +1,20 @@
-import axios, { AxiosRequestConfig } from "axios";
+/**
+ * Copyright (C) 2022-2023 Permanent Data Solutions, Inc. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import axiosRetry from "axios-retry";
 
 export interface CreateAxiosInstanceParams {
@@ -11,7 +27,7 @@ export const createAxiosInstance = ({
   config = {},
   retries = 8,
   retryDelay = axiosRetry.exponentialDelay,
-}: CreateAxiosInstanceParams) => {
+}: CreateAxiosInstanceParams): AxiosInstance => {
   const axiosInstance = axios.create(config);
   if (retries > 0) {
     axiosRetry(axiosInstance, {
