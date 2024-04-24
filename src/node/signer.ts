bfcdc4b13fc18313034c39f80511cb9b58396fb8
Derek Sonnenberg <derek@ardrive.io>
2024-04-24 14:54:32 -0500

    feat(upload): allow eth and sol signing for upload PE-5941

--- src/node/signer.ts
+++ src/node/signer.ts
@@ -17,6 +17,8 @@
 import {
   ArconnectSigner,
   ArweaveSigner,
+  EthereumSigner,
+  HexSolanaSigner,
   serializeTags,
   streamSigner,
 } from 'arbundles';
@@ -24,6 +26,7 @@ import { Readable } from 'node:stream';
 
 import { TurboDataItemAbstractSigner } from '../common/signer.js';
 import {
+  CreditableTokenType,
   DataItemOptions,
   StreamSizeFactory,
   TurboDataItemSignerParams,
@@ -33,12 +36,14 @@ import { fromB64Url } from '../utils/base64.js';
 /**
  * Utility exports to avoid clients having to install arbundles
  */
-export { ArconnectSigner, ArweaveSigner };
+export { ArconnectSigner, ArweaveSigner, EthereumSigner, HexSolanaSigner };
 
 /**
  * Node implementation of TurboDataItemSigner.
  */
-export class TurboNodeArweaveSigner extends TurboDataItemAbstractSigner {
+export class TurboNodeSigner extends TurboDataItemAbstractSigner {
+  token: CreditableTokenType;
+
   constructor(p: TurboDataItemSignerParams) {
     super(p);
   }
@@ -98,9 +103,8 @@ export class TurboNodeArweaveSigner extends TurboDataItemAbstractSigner {
     const serializedTags = tags && tags.length > 0 ? serializeTags(tags) : null;
     const tagsLength = 16 + (serializedTags ? serializedTags.byteLength : 0);
 
-    // Arweave sig and owner length is 512 bytes
-    const signatureLength = 512;
-    const ownerLength = 512;
+    const { ownerLength, signatureLength } = this.sigConfig;
+
     const signatureTypeLength = 2;
 
     return [
