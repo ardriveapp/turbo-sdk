a4ec4c23deac201f6b8744af8d943960dedbc8bb
Derek Sonnenberg <derek@ardrive.io>
2024-05-03 12:36:08 -0500

    docs(eth payments): add factory and top up examples PE-5992

--- src/common/factory.ts
+++ src/common/factory.ts
@@ -23,6 +23,7 @@ import {
   TurboSigner,
   TurboUnauthenticatedConfiguration,
   TurboWallet,
+  isEthPrivateKey,
   isJWK,
 } from '../types.js';
 import { TurboWebArweaveSigner } from '../web/signer.js';
@@ -84,7 +85,13 @@ export class TurboBaseFactory {
     } else if (providedPrivateKey !== undefined) {
       if (token === 'solana') {
         signer = new HexSolanaSigner(providedPrivateKey);
-        // TODO: else if (token === 'ethereum') {signer = new EthereumSigner(providedPrivateKey);}
+      } else if (token === 'ethereum') {
+        if (!isEthPrivateKey(providedPrivateKey)) {
+          throw new Error(
+            'An Ethereum private key must be provided for EthereumSigner.',
+          );
+        }
+        signer = new EthereumSigner(providedPrivateKey);
       } else {
         if (!isJWK(providedPrivateKey)) {
           throw new Error('A JWK must be provided for ArweaveSigner.');
