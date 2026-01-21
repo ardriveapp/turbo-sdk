ee365a7afb95c38ced5bca10f1e5e95c87ff6407
dtfiedler <dfieds913@gmail.com>
2026-01-21 10:44:11 -0600

    fix(deps): fix `update-version.sh` script with relative paths

--- packages/turbo-sdk/update-version.sh
+++ packages/turbo-sdk/update-version.sh
@@ -4,10 +4,10 @@
 nextRelease_version=$1
 
 # Update the version in src/version.ts
-sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" src/version.ts && rm src/version.ts.bak
+sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" ./src/version.ts && rm ./src/version.ts.bak
 
 # Update the version in lib/cjs/version.js
-sed -i.bak -e "s/exports.version = '.*';/exports.version = '${nextRelease_version}';/" lib/cjs/version.js && rm lib/cjs/version.js.bak
+sed -i.bak -e "s/exports.version = '.*';/exports.version = '${nextRelease_version}';/" ./lib/cjs/version.js && rm l./ib/cjs/version.js.bak
 
 # Update the version in lib/esm/version.js
-sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" lib/esm/version.js && rm lib/esm/version.js.bak
+sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" ./lib/esm/version.js && rm ./lib/esm/version.js.bak
