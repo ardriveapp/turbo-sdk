a744dcd4e38629e4bb01138fbad1d682dfa88340
dtfiedler <dfieds913@gmail.com>
2026-01-21 10:55:20 -0600

    chore(deps): add husky and fix `update_version.sh` script

--- packages/turbo-sdk/update-version.sh
+++ packages/turbo-sdk/update-version.sh
@@ -7,7 +7,7 @@ nextRelease_version=$1
 sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" ./src/version.ts && rm ./src/version.ts.bak
 
 # Update the version in lib/cjs/version.js
-sed -i.bak -e "s/exports.version = '.*';/exports.version = '${nextRelease_version}';/" ./lib/cjs/version.js && rm l./ib/cjs/version.js.bak
+sed -i.bak -e "s/exports.version = '.*';/exports.version = '${nextRelease_version}';/" ./lib/cjs/version.js && rm ./lib/cjs/version.js.bak
 
 # Update the version in lib/esm/version.js
 sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" ./lib/esm/version.js && rm ./lib/esm/version.js.bak
