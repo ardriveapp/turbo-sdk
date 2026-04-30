91e383886aed72c60f76f18356097eef7b34d012
Derek Sonnenberg <fedellen@pixelpajamastudios.com>
2026-04-30 10:45:45 -0500

    test: skip long running failure case submitfundtx test PE-9069

--- packages/turbo-sdk/tests/turbo.node.test.ts
+++ packages/turbo-sdk/tests/turbo.node.test.ts
@@ -576,7 +576,7 @@ describe('Node environment', () => {
         await mineArLocalBlock();
       });
 
-      it('should return a FailedRequestError when submitting a non-existent payment transaction ID', async () => {
+      it.skip('should return a FailedRequestError when submitting a non-existent payment transaction ID', async () => {
         const nonExistentPaymentTxId = 'non-existent-payment-tx-id';
         const error = await turbo
           .submitFundTransaction({ txId: nonExistentPaymentTxId })
